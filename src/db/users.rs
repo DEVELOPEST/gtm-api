d2fc713ff6901413189985aaac42a804b67a80ad
Marten Jürgenson <mjurge@ttu.ee>
2020-11-21 23:59:08 +0200

    API-4 User creation functionality

---
+++ src/db/users.rs
@@ -0,0 +1,59 @@
+use crate::models::user::User;
+use crate::schema::users;
+use crypto::scrypt::{scrypt_check, scrypt_simple, ScryptParams};
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use diesel::result::{DatabaseErrorKind, Error};
+use serde::Deserialize;
+use diesel::{Insertable};
+
+#[derive(Insertable)]
+#[table_name = "users"]
+pub struct NewUser<'a> {
+    pub email: &'a str,
+    pub hash: &'a str,
+}
+
+pub enum UserCreationError {
+    DuplicatedEmail,
+    DuplicatedUsername,
+}
+
+impl From<Error> for UserCreationError {
+    fn from(err: Error) -> UserCreationError {
+        if let Error::DatabaseError(DatabaseErrorKind::UniqueViolation, info) = &err {
+            match info.constraint_name() {
+                Some("users_username_key") => return UserCreationError::DuplicatedUsername,
+                _ => {}
+            }
+        }
+        panic!("Error creating user: {:?}", err)
+    }
+}
+
+pub fn create(
+    conn: &PgConnection,
+    email: &str,
+    password: &str,
+) -> Result<User, UserCreationError> {
+    // see https://blog.filippo.io/the-scrypt-parameters
+    let hash = &scrypt_simple(password, &ScryptParams::new(14, 8, 1)).expect("hash error");
+
+    let new_user = &NewUser {
+        email,
+        hash,
+    };
+
+    diesel::insert_into(users::table)
+        .values(new_user)
+        .get_result::<User>(conn)
+        .map_err(Into::into)
+}
+
+pub fn find(conn: &PgConnection, id: i32) -> Option<User> {
+    users::table
+        .find(id)
+        .get_result(conn)
+        .map_err(|err| println!("find_user: {}", err))
+        .ok()
+}
