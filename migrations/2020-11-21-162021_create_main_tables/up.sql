a3f130f1259db90e336c4fa8865a9d16783b811c
mjurge <Amkisnuim1>
2020-11-21 19:33:22 +0200

    API-3 Add migrations

---
+++ migrations/2020-11-21-162021_create_main_tables/up.sql
@@ -0,0 +1,46 @@
+-- Your SQL goes here
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email TEXT NOT NULL UNIQUE,
+  hash TEXT NOT NULL
+);
+
+CREATE TABLE tokens (
+  id SERIAL PRIMARY KEY,
+  "user" INTEGER REFERENCES users ON DELETE CASCADE,
+  access_token TEXT NOT NULL,
+  added_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE git_groups (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  added_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE repositories (
+  id SERIAL PRIMARY KEY,
+  url TEXT NOT NULL,
+  sync_url TEXT NOT NULL,
+  access_token TEXT NOT NULL,
+  added_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE commits (
+  id SERIAL PRIMARY KEY,
+  repository INTEGER REFERENCES repositories ON DELETE CASCADE,
+  hash TEXT NOT NULL,
+  message TEXT NOT NULL,
+  email TEXT NOT NULL,
+  timestamp TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE files (
+  id SERIAL PRIMARY KEY,
+  commit INTEGER REFERENCES commits ON DELETE CASCADE,
+  name TEXT NOT NULL,
+  time BIGINT NOT NULL,
+  lines_added BIGINT NOT NULL,
+  lines_deleted BIGINT NOT NULL,
+  lines_changed BIGINT NOT NULL
+);
