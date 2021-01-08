723a488174c23906ce675aa378a9d79072102344
Marten Jürgenson <mjurge@ttu.ee>
2021-01-08 18:15:24 +0200

    API-10 Add endpoint for day info

--- src/routes/timelines.rs
+++ src/routes/timelines.rs
@@ -1,8 +1,25 @@
 use serde::Deserialize;
 use validator::Validate;
+use crate::routes::files::NewFileData;
+use crate::db;
+use rocket_contrib::json::{JsonValue};
+use rocket::request::Form;
+use crate::db::timelines::{Period};
+
 
 #[derive(Deserialize, Validate)]
 pub struct NewTimelineData {
     pub timestamp: Option<i64>,
     pub time: Option<i64>,
+}
+
+#[get("/<repo>/timeline?<params..>")]
+pub fn get_day_timeline(
+    //auth: Auth,
+    repo: String,
+    params: Form<Period>,
+    conn: db::Conn,
+) -> JsonValue {
+    let day_timeline = db::timelines::get_day(&conn, &repo, &params);
+    json!({ "timeline": day_timeline })
 }
