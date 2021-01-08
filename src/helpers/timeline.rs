6cfac6ccc05158eac0fbcddae3258b6b3b61c1b9
Marten Jürgenson <mjurge@ttu.ee>
2021-01-08 18:23:01 +0200

    API-10 Add missing mappers and helpers

---
+++ src/helpers/timeline.rs
@@ -0,0 +1,19 @@
+use crate::models::timeline::HourData;
+
+pub fn generate_hour_base_data(
+    start: i64,
+    end: i64
+) -> Vec<HourData> {
+    let mut hour_data = Vec::new();
+    let hour_in_seconds = 3600;
+    for x in 0..24 {
+        hour_data.push(HourData {
+            start: start + hour_in_seconds * x,
+            end: start + hour_in_seconds * (x + 1),
+            hour: x as i32,
+            time: 0,
+            users: Vec::new()
+        });
+    }
+    hour_data
+}
