ff670b3177f4d2f9b34196c560fc48a2e72c2511
tavo <tavo.annus@gmail.com>
2021-01-16 17:12:16 +0200

    Implement fetching by groups for timeline

--- src/routes/timelines.rs
+++ src/routes/timelines.rs
@@ -4,7 +4,6 @@ use validator::Validate;
 use crate::db;
 use rocket_contrib::json::{JsonValue};
 use rocket::request::Form;
-use crate::models::repository::Repository;
 
 
 #[derive(Deserialize, Validate)]
@@ -44,7 +43,7 @@ pub fn get_timeline(
 
 #[get("/<group_name>/test")]
 pub fn get_group(group_name: String, conn: db::Conn) -> JsonValue {
-    let repos: Vec<_> = db::repositories::find_all_repositories(&conn, &group_name);
+    let repos: Vec<_> = db::repositories::find_all_repositories_in_group(&conn, &group_name);
     let result: Vec<i32> = repos.iter().map(|r| r.id).collect();
     json!(result)
 }
