17267fb5e96427b6402fea331b43fcac16b6a27c
tavo <tavo.annus@gmail.com>
2021-01-19 22:10:01 +0200

    Pr fixes

--- src/db/timelines.rs
+++ src/db/timelines.rs
@@ -91,20 +91,6 @@ pub fn get_timeline(
         .load(conn)
         .expect("Error loading timeline for group");
 
-    // let day_timeline = timeline::table
-    //     .inner_join(files::table)
-    //     .inner_join(commits::table.on(files::commit.eq(commits::id)))
-    //     .inner_join(repositories::table.on(repositories::id.eq(commits::repository_id)))
-    //     // TODO(Tavo): Recursive join group_group_member
-    //     // .inner_join(group_group_members::table.on(group_group_members::repository.eq(repositories::id)))
-    //     .inner_join(groups::table.on(groups::id.eq(repositories::group)))
-    //     .filter(groups::name.eq(group_name)
-    //         .and(timeline::timestamp.ge(start)
-    //             .and(timeline::timestamp.lt(end))))
-    //     .order(timeline::timestamp.asc())
-    //     .select((repositories::user, timeline::time, timeline::timestamp ))
-    //     .load::<TimelineDWH>(conn)
-    //     .expect("Cannot get day timeline");
     println!("{:?}", day_timeline);
     map_timeline(day_timeline, start, end, timezone, interval)
 }
