854facbcaedf3b474019800b97254044f6b17f04
tavo <tavo.annus@gmail.com>
2021-01-20 22:01:46 +0200

    Start work on activity timeline

--- src/mappers/timeline.rs
+++ src/mappers/timeline.rs
@@ -1,10 +1,10 @@
 use chrono_tz::Tz;
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Utc, Timelike};
 
 use std::time::{UNIX_EPOCH, Duration};
 
 use crate::helpers::timeline::generate_intervals;
-use crate::models::interval::IntervalJson;
+use crate::models::interval::{IntervalJson, ActivityJson, Activity};
 use crate::models::timeline_dwh::TimelineDWH;
 
 pub fn map_timeline(
@@ -31,6 +31,41 @@ pub fn map_timeline(
     }
     
     intervals.into_iter().map(|x| x.attach()).collect()
+}
+
+pub fn map_activity(
+    data: Vec<TimelineDWH>,
+    start: i64,
+    end: i64,
+    timezone: &str,
+    interval: &str,
+) -> Vec<ActivityJson> {
+    let tz: Tz = timezone.parse().unwrap();
+    let start_tz: DateTime<Tz> = get_datetime_tz_from_seconds(start, &tz);
+    let end_tz = get_datetime_tz_from_seconds(end, &tz);
+
+    let mut interval : Vec<Activity> = vec![];
+    for i in 0..24 {
+        interval.push(Activity{
+            id: i,
+            label: format!("{}", i),
+            time: 0,
+            lines_added: 0,
+            lines_removed: 0,
+            users: vec![]
+        })
+    }
+
+    for item in data {
+        let time_point = get_datetime_tz_from_seconds(item.timestamp, &tz);
+        let i = interval.iter().position(|a| a.id == time_point.hour() as i32).unwrap();
+        interval[i].time += item.time;
+        if !interval[i].users.contains(&item.user) {
+            interval[i].users.push(item.user);
+        }
+    }
+
+    interval.into_iter().map(|x| x.attach()).collect()
 }
 
 pub fn get_datetime_tz_from_seconds(seconds: i64, timezone: &Tz) -> DateTime<Tz> {
