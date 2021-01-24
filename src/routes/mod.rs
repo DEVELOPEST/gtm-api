32f1fbd7c233e8c675aa1ace2c6a895853fe782a
MartenJurg <47556960+MartenJurg@users.noreply.github.com>
2021-01-24 19:57:36 +0200

    API-17 Redesing to domain driven architecture (#13)
    * API-17 Redesing to domain driven architecture
    
    * API-17 Redesing to domain driven architecture
    
    Co-authored-by: mjurge <marten.jurg@hotmail.com>

--- src/routes/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod users;
-pub mod commits;
-pub mod repositories;
-pub mod timelines;
-pub mod files;
-pub mod groups;
-pub mod auth;
---
+++ src/timeline/mod.rs
@@ -0,0 +1,8 @@
+pub mod service;
+pub mod routes;
+pub mod model;
+pub mod mapper;
+pub mod helper;
+pub mod db;
+pub mod dwh;
+pub mod resources;
