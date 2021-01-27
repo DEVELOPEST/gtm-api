179c23b75449115cea878e8d4b0a1abab9205422
tavo <tavo.annus@gmail.com>
2021-01-27 10:02:15 +0200

    Fix err 500 on no commits

--- src/commit/routes.rs
+++ src/commit/routes.rs
@@ -1,10 +1,12 @@
+use rocket_contrib::json::JsonValue;
 use serde::Deserialize;
 use validator::Validate;
-use rocket_contrib::json::{JsonValue};
-use crate::file::routes::NewFileData;
-use crate::db::Conn;
-use crate::repository;
+
 use crate::commit;
+use crate::commit::model::Commit;
+use crate::db::Conn;
+use crate::file::routes::NewFileData;
+use crate::repository;
 
 #[derive(Deserialize, Validate)]
 pub struct NewCommitData {
@@ -31,7 +33,16 @@ pub fn get_commit_hash(
     conn: Conn,
 ) -> JsonValue {
     let repository = repository::db::find(&conn, &user, &provider, &repo);
-    let last_commit = commit::db::find_last_by_repository_id(&conn, repository.id);
+    let last_commit = commit::db::find_last_by_repository_id(&conn, repository.id)
+        .unwrap_or(Commit {
+            id: 0,
+            repository_id: 0,
+            hash: "".to_string(),
+            message: "".to_string(),
+            author: "".to_string(),
+            branch: "".to_string(),
+            time: 0,
+        });
     let hashes = commit::db::find_all_by_repository_id(&conn, repository.id);
     json!({
     "hash": last_commit.hash ,
