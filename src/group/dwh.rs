0840e87dca6f0e5fda72df7543c64e0926aa7086
tavo <tavo.annus@gmail.com>
2021-02-12 21:00:39 +0200

    Add group files stats

--- src/group/dwh.rs
+++ src/group/dwh.rs
@@ -2,7 +2,7 @@ use diesel::sql_types::{BigInt, Text};
 use serde::Serialize;
 
 #[derive(QueryableByName, Debug, Serialize)]
-pub struct GroupRepoStats {
+pub struct GroupUserStats {
     #[sql_type = "Text"]
     pub name: String,
     #[sql_type = "BigInt"]
@@ -13,4 +13,26 @@ pub struct GroupRepoStats {
     pub lines_removed: i64,
     #[sql_type = "BigInt"]
     pub commits: i64,
+}
+
+#[derive(QueryableByName, Debug, Serialize)]
+pub struct GroupFileStats {
+    #[sql_type = "Text"]
+    pub path: String,
+    #[sql_type = "BigInt"]
+    pub total_time: i64,
+    #[sql_type = "BigInt"]
+    pub lines_added: i64,
+    #[sql_type = "BigInt"]
+    pub lines_removed: i64,
+    #[sql_type = "BigInt"]
+    pub commits: i64,
+    #[sql_type = "BigInt"]
+    pub users: i64,
+}
+
+#[derive(Debug, Serialize)]
+pub struct GroupStats {
+    pub users: Vec<GroupUserStats>,
+    pub files: Vec<GroupFileStats>,
 }
