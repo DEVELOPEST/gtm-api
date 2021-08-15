4ff1f19e35006f720f4f754793d91f0f9cd22608
Tavo Annus <tavo.annus@gmail.com>
2021-08-15 22:01:34 +0300

    Run tests on single thread

--- src/setup.rs
+++ src/setup.rs
@@ -9,8 +9,8 @@ pub fn migrate_database() -> AdHoc {
         let conn = Conn::get_one(&rocket).expect("database connection");
         match embedded_migrations::run(&*conn) {
             Ok(()) => Ok(rocket),
-            Err(_) => {
-                // error!("Failed to run database migrations: {:?}", e);
+            Err(e) => {
+                error!("Failed to run database migrations: {:?}", e);
                 Err(rocket)
             }
         }
